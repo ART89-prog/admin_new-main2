7f082cf47210937483841f9dc452da101f2a8af1
ART89-prog <dykpgilkom@mail.ru>
2023-09-16 18:16:42 +0300

    5654652

--- js/simulator.js
+++ js/simulator.js
@@ -1,4 +1,47 @@
 $(document).ready(function() {
+
+    $('body').on('click', '.steps_stage .step_stage:not(.active)', function (e) {
+        var date = new Date();
+        $(this).addClass("active").find("use").attr("xlink:href","images/sprite.svg#training_ok");
+        let min = date.getMinutes();
+        if(min<10)
+        {
+            min = "0"+min;
+        }
+        $(this).find(".step_stage_time").text("Выполнено в " +  date.getHours() + ":"+  min);
+
+        if($(".steps_stage .step_stage:not(.active)").length==0)
+        {
+            let id = $(this).closest(".steps_data_js").data("id");
+            $(".inner_steps .step[data-id='"+id+"']").removeClass("error").addClass("complete").find("use").attr("xlink:href","images/sprite.svg#step_complete");
+        }
+    });
+
+
+    $(".inner_steps .step").click(function (e) {
+        e.preventDefault();
+        let id = $(this).data("id");
+        $(".steps_data_js").fadeOut(200).removeClass("active");
+        setTimeout(() => {
+            $(".steps_data_js[data-id='"+id+"']").fadeIn(200).addClass("active");
+        }, 200);
+
+        $(".comments_data_js").fadeOut(200);
+        setTimeout(() => {
+            $(".comments_data_js[data-id='"+id+"']").fadeIn(200);
+        }, 200);
+
+        setTimeout(() => {    
+            if($(window).width()>1023)
+            {        
+                const elHeight = $(".data_with_sidebar .data.active").outerHeight();
+                $(".data_with_sidebar .steps").outerHeight(elHeight);
+            }
+        }, 500);
+    });
+
+
+
     setTimeout(() => {
         if($(window).width()>1023)
         {
@@ -6,7 +49,7 @@ $(document).ready(function() {
             $(".data_with_sidebar .steps").outerHeight(elHeight);
 
 
-            if($(".data_with_sidebar .steps").length>0)
+            /*if($(".data_with_sidebar .steps").length>0)
             {   
                 
                     var hash = window.location.hash+"00";
@@ -16,12 +59,12 @@ $(document).ready(function() {
                         $('.inner_steps').animate({scrollTop: top}, 800); // плавно переходим к блоку
                     }
                
-            }    
+            }   */ 
         }
         else
         {
 
-            if($(".data_with_sidebar .steps").length>0)
+            /*if($(".data_with_sidebar .steps").length>0)
             {                   
                 var hash = window.location.hash+"00";
                 if(hash)
@@ -30,9 +73,9 @@ $(document).ready(function() {
                     $('.inner_steps').animate({scrollLeft: left }, 800); // плавно переходим к блоку
                 }
                
-            }    
+            }  */  
         }
-    }, 1000)
+    }, 0)
 
    
 });
@@ -45,7 +88,7 @@ $(window).on('resize', () => {
         $(".data_with_sidebar .steps").outerHeight(elHeight);
 
 
-        if($(".data_with_sidebar .steps").length>0)
+        /*if($(".data_with_sidebar .steps").length>0)
         {   
                 
                 var hash = window.location.hash+"00";
@@ -55,7 +98,7 @@ $(window).on('resize', () => {
                     $('.inner_steps').animate({scrollTop: top}, 800); // плавно переходим к блоку
                 }
            
-        }    
+        }   */ 
     }
     else
     {
